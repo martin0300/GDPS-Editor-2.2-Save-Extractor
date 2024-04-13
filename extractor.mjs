c75e12462eab53a3ce2bfcd0f053931aed597b28
Martin Magyar <magyar.martintin@gmail.com>
2024-04-13 17:30:41 +0200

    remove express server

--- extractor.mjs
+++ extractor.mjs
@@ -1,15 +1,12 @@
 import socksv5 from "socksv5";
 const { createServer } = socksv5;
 
-import express from "express";
 import fs from "fs";
 import zlib from "zlib";
 import ip from "ip";
 
-const app = express();
 const debug = true;
 const proxyPort = 9999;
-const serverPort = 80;
 const ipAddress = ip.address();
 
 function init() {
@@ -37,40 +34,6 @@ function decodeSaveData(saveData) {
     return;
 }
 
-app.post("/server/getAccountURL.php", (req, res) => {
-    console.log("Got connection!");
-    res.status(200).send("http://game.gdpseditor.com");
-});
-
-app.post("/database/accounts/backupGJAccountNew.php", express.urlencoded({ extended: true, limit: "50mb" }), (req, res) => {
-    console.log("Getting data...");
-    console.log(req.body);
-    /*decodeSaveData(req.body.saveData);
-    res.sendStatus(200);
-    console.log("Finished");
-    console.log("Save data saved to saveFiles folder!");
-    process.exit(0);*/
-});
-
-/*app.use((err, req, res, next) => {
-    console.error(err.stack);
-});
-
-app.use((req, res, next) => {
-    console.log("Received request:");
-    console.log("Request Method:", req.method);
-    console.log("Request URL:", req.url);
-    console.log("Request Headers:", req.headers);
-    console.log("Request Body:", req.body); // For POST or other request methods with a request body
-    console.log("-----------------------------------------");
-    next(); // Continue processing the request
-});
-
-// Define a route that responds with a 200 OK for all requests
-app.all("*", (req, res) => {
-    res.status(404).send("OK");
-});*/
-
 init();
 
 //proxy
@@ -82,7 +45,7 @@ const server = createServer((info, accept, deny) => {
     if (info.dstPort == 80) {
         var socket;
         if (debug) {
-            console.log("Redirecting to interceptor server");
+            console.log("Reading data");
         }
         if ((socket = accept(true))) {
             //404 response
@@ -156,10 +119,6 @@ server.listen(proxyPort, ipAddress, () => {
     console.log(`SOCKS Proxy listening on port ${proxyPort}`);
     console.log(`Server address: ${ipAddress}`);
     console.log(`Server port: ${proxyPort}`);
-    //start interceptor server
-    app.listen(serverPort, () => {
-        console.log("Waiting for connection...");
-    });
 });
 
 server.useAuth(socksv5.auth.None());
