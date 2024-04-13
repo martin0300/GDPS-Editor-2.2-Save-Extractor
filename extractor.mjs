0378192bca6abd71b2bb33ab318bac9034d1290b
Martin Magyar <magyar.martintin@gmail.com>
2024-04-13 17:29:17 +0200

    new socket server

--- extractor.mjs
+++ extractor.mjs
@@ -85,34 +85,67 @@ const server = createServer((info, accept, deny) => {
             console.log("Redirecting to interceptor server");
         }
         if ((socket = accept(true))) {
+            //404 response
             const failBody = "Not found";
             const failResponse = ["HTTP/1.1 404 Not found", "Connection: close", "Content-Type: text/plain", "Content-Length: " + Buffer.byteLength(failBody), "", failBody].join("\r\n");
 
+            //getAccountURL
             const getAccountURLBody = "http://game.gdpseditor.com";
-            var getAccountURLResponse = ["HTTP/1.1 200 OK", "Connection: close", "Content-Type: text/plain", "Content-Length: " + Buffer.byteLength(getAccountURLBody), "", getAccountURLBody].join(
+            var getAccountURLResponse = ["HTTP/1.1 200 OK", "Connection: close", "Content-Type: text/html", "Content-Length: " + Buffer.byteLength(getAccountURLBody), "", getAccountURLBody].join(
                 "\r\n"
             );
 
+            //continue message
+            const backupGJAccountNewStage1Response = "HTTP/1.1 100 Continue\r\n\r\n";
+
+            //backup success
+            const backupGJAccountNewSuccessBody = "OK";
+            const backupGJAccountNewSuccessResponse = [
+                "HTTP/1.1 200 OK",
+                "Connection: close",
+                "Content-Type: text/html",
+                "Content-Length: " + Buffer.byteLength(backupGJAccountNewSuccessBody),
+                "",
+                backupGJAccountNewSuccessBody,
+            ].join("\r\n");
+
+            var saveDataBuffer = "";
+
             socket.on("data", (data) => {
                 var requestData = data.toString();
-                console.log(requestData);
+                //console.log(requestData);
                 var requestLines = requestData.split("\r\n");
 
-                if (requestLines[1].includes("game.gdpseditor.com")) {
+                if (requestLines.length > 1 && requestLines[1].includes("game.gdpseditor.com")) {
                     if (requestLines[0].includes("/server/getAccountURL.php")) {
                         if (debug) {
                             console.log("Sending getAccountURL response...");
                         }
+                        console.log("Got connection");
                         socket.end(getAccountURLResponse);
+                    } else if (requestLines[0].includes("/database/accounts/backupGJAccountNew.php")) {
+                        if (debug) {
+                            console.log("Sending continue...");
+                        }
+                        socket.write(backupGJAccountNewStage1Response);
                     } else {
                         socket.end(failResponse);
+                    }
+                } else if (requestLines.length == 1) {
+                    if (debug) {
+                        console.log("Got new chunk");
+                    }
+                    saveDataBuffer += requestData;
+                    if (requestData.includes("secret=Wmfv3899gc9")) {
+                        if (debug) {
+                            console.log("End of transmission");
+                        }
                     }
                 } else {
                     socket.end(failResponse);
                 }
             });
         }
-        info.dstAddr = "127.0.0.1";
     } else {
         accept();
     }
