fdc0d450a4a7c0c3f0cb2ce07e3da65a884c7364
Martin Magyar <magyar.martintin@gmail.com>
2025-05-27 23:12:32 +0200

    add start batch files

---
+++ start.bat
@@ -0,0 +1,23 @@
+@echo off
+
+echo Checking for node installation...
+
+where node >nul 2>nul
+if errorlevel 1 (
+    echo Node.js is not installed.
+    echo Please install it from here: https://nodejs.org/
+    pause
+    exit /b
+)
+
+if exist "node_modules\" (
+    echo Skipping dependency installation.
+) else (
+    echo Installing dependencies...
+    npm i
+)
+
+node extractor.mjs %1
+
+pause
+exit /b
